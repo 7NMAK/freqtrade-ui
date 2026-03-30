c3bfccfb8eb4c2c6e0110945336d862c5e75e96f
Novakus <nk@7nmak.com>
2026-03-30 19:49:46 +0200

    deploy: 2026-03-30_19:49

--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -12,7 +12,7 @@ function Card({
       data-slot="card"
       data-size={size}
       className={cn(
-        "group/card flex flex-col gap-4 overflow-hidden rounded-xl bg-card py-4 text-sm text-card-foreground ring-1 ring-foreground/10 has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:gap-3 data-[size=sm]:py-3 data-[size=sm]:has-data-[slot=card-footer]:pb-0 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl",
+        "group/card flex flex-col gap-3 overflow-hidden rounded-[10px] bg-card py-3 text-sm text-card-foreground ring-1 ring-foreground/10 has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:gap-2 data-[size=sm]:py-2 data-[size=sm]:has-data-[slot=card-footer]:pb-0 *:[img:first-child]:rounded-t-[10px] *:[img:last-child]:rounded-b-[10px]",
         className
       )}
       {...props}
@@ -40,7 +40,7 @@ function CardHeader({ className, title, icon, action, ...props }: React.Componen
     <div
       data-slot="card-header"
       className={cn(
-        "group/card-header @container/card-header flex flex-col space-y-1.5 p-6",
+        "group/card-header @container/card-header flex flex-col space-y-1.5 p-4",
         className
       )}
       {...props}
